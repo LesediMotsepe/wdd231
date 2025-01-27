ea9ca8947393a6a38a4517e5ee3a07d2f574a9c4
lesedimotsepe2 <lesedimotsepe2@>
2025-01-27 18:18:18 +0200

    Created the join html page

--- chamber/scripts/fav.js
+++ chamber/scripts/fav.js
@@ -51,3 +51,54 @@ listViewButton.addEventListener('click', () => {
 
 
 displayMembers();
+
+const spotlightCards = document.getElementById('spotlight-cards');
+
+
+fetch('data/members.json')
+    .then(response => response.json())
+    .then(data => {
+       
+        const goldAndSilverMembers = data.filter(member => member.membershipLevel === 2 || member.membershipLevel === 3);
+
+     
+        const selectedMembers = goldAndSilverMembers.sort(() => Math.random() - 0.5).slice(0, Math.floor(Math.random() * 2) + 2);
+
+        selectedMembers.forEach(member => {
+            const spotlightCard = document.createElement('div');
+            spotlightCard.classList.add('spotlight-card');
+            spotlightCard.innerHTML = `
+                <img src="images/${member.image}" alt="${member.name}">
+                <h3>${member.name}</h3>
+                <p>${member.address}</p>
+                <p>${member.phone}</p>
+                <p><a href="${member.website}">${member.website}</a></p>
+                <p>Membership Level: ${member.membershipLevel === 2 ? 'Silver' : 'Gold'}</p>
+            `;
+            spotlightCards.appendChild(spotlightCard);
+        });
+    })
+    .catch(error => console.error('Error loading member data:', error));
+    
+const membershipCards = document.querySelector('.membership-cards');
+membershipCards.classList.add('animate');
+
+
+const links = document.querySelectorAll('.card a');
+links.forEach(link => {
+    link.addEventListener('click', event => {
+        event.preventDefault();
+        const modalId = link.dataset.modal;
+        const modal = document.getElementById(modalId);
+        modal.style.display = 'block';
+    });
+});
+const closeButtons = document.querySelectorAll('.close');
+closeButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = button.parentNode.parentNode;
+        modal.style.display = 'none';
+    });
+});
+
+
