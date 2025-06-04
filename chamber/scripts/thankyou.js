8394d218e1004c6ad1e25c85fab6f5056a2b578b
LesediMotsepe <105650490+LesediMotsepe@users.noreply.github.com>
2025-06-04 11:05:03 +0200

    Update thankyou.js

--- chamber/scripts/thankyou.js
+++ chamber/scripts/thankyou.js
@@ -1,1 +1,13 @@
 
+document.addEventListener("DOMContentLoaded", () => {
+  const data = JSON.parse(localStorage.getItem("formData"));
+
+  if (data) {
+    document.getElementById("fName").textContent = data.firstName;
+    document.getElementById("lName").textContent = data.lastName;
+    document.getElementById("email").textContent = data.email;
+    document.getElementById("phone").textContent = data.phone;
+    document.getElementById("business").textContent = data.business;
+    document.getElementById("date").textContent = data.date;
+  }
+});
