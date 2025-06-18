da93cd01d2aebd30574c45069d87d5b232b21df2
LesediMotsepe <105650490+LesediMotsepe@users.noreply.github.com>
2025-06-18 05:55:53 +0200

    Add files via upload

---
+++ finalproject/treats.js
@@ -0,0 +1,10 @@
+export async function loadTreats() {
+  try {
+    const response = await fetch('../data.json');
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error loading treats:', error);
+    return [];
+  }
+}
