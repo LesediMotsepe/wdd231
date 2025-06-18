d36ff5aacce5d706949bfbfb65c58afdec3a7e7f
LesediMotsepe <105650490+LesediMotsepe@users.noreply.github.com>
2025-06-18 05:49:05 +0200

    Delete finalproject/scripts/script.js

--- finalproject/scripts/script.js
+++
@@ -1,3 +0,0 @@
-// script.js for Sweet Treats
-document.getElementById("year").textContent = new Date().getFullYear();
-document.getElementById("modified").textContent = "Last modified: " + document.lastModified;
