5db1a220d71829c6cd7f210b9313c699704e88bb
LesediMotsepe <105650490+LesediMotsepe@users.noreply.github.com>
2025-06-18 03:20:06 +0200

    Create script.js

---
+++ finalproject/scripts/script.js
@@ -0,0 +1,3 @@
+// script.js for Sweet Treats
+document.getElementById("year").textContent = new Date().getFullYear();
+document.getElementById("modified").textContent = "Last modified: " + document.lastModified;
