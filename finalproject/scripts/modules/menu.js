6c9001cf69a3abff5b435b1aebe651b3b0ad8543
LesediMotsepe <105650490+LesediMotsepe@users.noreply.github.com>
2025-06-19 23:19:11 +0200

    Update menu.js

--- finalproject/scripts/modules/menu.js
+++ finalproject/scripts/modules/menu.js
@@ -1,11 +1,11 @@
-// Wait until the DOM is fully loaded
+
 document.addEventListener("DOMContentLoaded", () => {
   const toggleButton = document.getElementById("menu-toggle");
   const navMenu = document.getElementById("menu");
 
   if (toggleButton && navMenu) {
     toggleButton.addEventListener("click", () => {
-      navMenu.classList.toggle("show"); // Add or remove the 'show' class
+      navMenu.classList.toggle("show"); 
     });
   }
 });
