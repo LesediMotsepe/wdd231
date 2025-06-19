4987a1024e79dc8de621043ffad4362d37d6ccb2
LesediMotsepe <105650490+LesediMotsepe@users.noreply.github.com>
2025-06-19 23:21:04 +0200

    Update treats.js

--- finalproject/scripts/modules/treats.js
+++ finalproject/scripts/modules/treats.js
@@ -8,3 +8,45 @@ export async function loadTreats() {
     return [];
   }
 }
+export async function loadTreats() {
+  return [
+    {
+      name: "Strawberry Cheesecake",
+      flavor: "Strawberry",
+      price: "$2.45",
+      image: "images/treat1.jpg"
+    },
+    {
+      name: "Chocolate Fudge Brownie",
+      flavor: "Chocolate",
+      price: "$2.10",
+      image: "images/treat2.jpg"
+    },
+    {
+      name: "Vanilla Cupcake",
+      flavor: "Vanilla",
+      price: "$1.80",
+      image: "images/treat3.jpg"
+    },
+    {
+      name: "Lemon Tart",
+      flavor: "Lemon",
+      price: "$2.30",
+      image: "images/treat4.jpg"
+    },
+    {
+      name: "Mint Chocolate Chip Ice Cream",
+      flavor: "Mint Chocolate",
+      price: "$2.50",
+      image: "images/treat5.jpg"
+    },
+    {
+      name: "Carrot Cake Slice",
+      flavor: "Carrot",
+      price: "$2.20",
+      image: "images/treat6.jpg"
+    }
+   
+  ];
+}
+
