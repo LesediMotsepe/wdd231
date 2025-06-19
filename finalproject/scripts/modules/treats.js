b206286925caceb38b6aab74166e85d8c53b471c
LesediMotsepe <105650490+LesediMotsepe@users.noreply.github.com>
2025-06-19 23:26:22 +0200

    Update treats.js

--- finalproject/scripts/modules/treats.js
+++ finalproject/scripts/modules/treats.js
@@ -8,7 +8,7 @@ export async function loadTreats() {
     return [];
   }
 }
-export async function loadTreats() {
+export async function loadmenu() {
   return [
     {
       name: "Strawberry Cheesecake",
